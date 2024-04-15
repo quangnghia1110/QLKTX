dec6500c7de478760a8516fdd3da820a19d66ab6
Ngo Quang Nghia <21110559@st.hcmute.edu.vn>
2024-04-15 15:07:42 +0700

    update authentication

---
+++ src/Dao/ThongKeDAO.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Dao;
+
+/**
+ *
+ * @author hoang
+ */
+
+import java.util.List;
+import Model.Student;
+
+public interface ThongKeDAO {
+    
+    public List<Student> getListByStudent();
+    
+    //public List<Student> getListByStudentForPie();
+    
+}
---
+++ src/Model/ThongKeServiceImpl.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author hoang
+ */
+import Dao.ThongKeDAOImpl;
+import Dao.ThongKeDAO;
+import java.util.List;
+
+public class ThongKeServiceImpl implements ThongKeService {
+
+    private ThongKeDAO thongKeDAO = null;
+
+    public ThongKeServiceImpl() {
+        this.thongKeDAO = new ThongKeDAOImpl();
+    }
+
+    @Override
+    public List<Student> getListByStudent() {
+        return thongKeDAO.getListByStudent();
+    }
+    
+    /*public List<Student> getListByStudentForPie() {
+        return thongKeDAO.getListByStudent();
+    }*/
+
+}
